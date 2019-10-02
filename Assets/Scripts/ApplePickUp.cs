804a04467d172f52441633cbffe74d3d337036fb
Nick Petho <nick.petho@gmail.com>
2019-10-02 01:56:43 -0400

    Implemented PacMan animation

---
+++ Assets/Scripts/ApplePickUp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplePickUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DestroyGameObject()
+    {
+        Destroy(gameObject);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        DestroyGameObject();  
+    }
+}
---
+++ Assets/Scripts/PacManMove.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// This script uses keyboard events to move the object.
+public class PacManMove : MonoBehaviour
+{
+    public float speed = 0.01f;
+    public bool isCaught = false;
+    Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Get the Rigidbody component of this object
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Amount to move in each dimension
+        float dx = 0;
+        float dy = 0;
+
+        // Input.GetKey(key) returns true if that key is currently down
+        // KeyCode.keyname is used for keys that don'w correspond to an ASCII char
+
+        // Move up
+        if (Input.GetKey("w") || Input.GetKey(KeyCode.UpArrow))
+        {
+            dy = speed;
+            transform.eulerAngles = new Vector3(0, 0, 90);
+        }
+        // Move down
+        if (Input.GetKey("s") || Input.GetKey(KeyCode.DownArrow))
+        {
+            dy = -speed;
+            transform.eulerAngles = new Vector3(0, 0, 270);
+        }
+        // Move left
+        if (Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow))
+        {
+            dx = -speed;
+            transform.eulerAngles = new Vector3(0, 0, 180);
+        }
+        // Move right
+        if (Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow))
+        {
+            dx = speed;
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+
+        // Move by that amount
+        rb.position += new Vector2(dx, dy);
+    }
+}
